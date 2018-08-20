5812c9d424c8f6d37ec1759b7c50ce49eb763f8e
Aye-Thet <ayethet107@gmail.com>
2018-08-20 18:06:28 +0630

    Updated and Delete

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -19,4 +19,32 @@ router.get('/view/:id', function(req, res, next){
   });
 });
 
+router.get('/modify/:uid', function(req, res, next){
+  User.findById(req.params.uid, function(err, users) {
+    if (err) throw err;
+    if(users.length == 0) next(new Error('User data not Found!'));
+    res.render('admin/users/user-modify', {title: 'Modify User', user: users[0]});
+  });
+});
+
+router.post('/modify', function(req, res, next) {
+ var params = [req.body.name, req.body.role, req.body.uid];
+ User.findById( req.body.uid,function(err, user) {
+   if (err) throw err;
+   if(user.length == 0) next(new Error('User data not found!'));
+   User.update(params, function(uerr,uuser){
+     if(uerr) throw uerr;
+     req.flash('info', 'Success');
+     res.redirect('/admin/users/view/'+ user[0].uid);
+   });
+ });
+});
+router.post('/remove', function(req, res, next) {
+  User.remove(req.body.uid,function(err, user) {
+      if (err) throw err;
+      req.flash('info', 'Successfully');
+    res.redirect('/admin/users/list');
+  });
+});
+
 module.exports = router;
