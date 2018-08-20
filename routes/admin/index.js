059df344fe49d91569d7cf3844e8e61db770dc49
Aye-Thet <ayethet107@gmail.com>
2018-08-20 12:05:52 +0630

    Add serach box in user list

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var users = require('./users');
 // admin role check
 router.use(function(req, res, next) {
   if(req.session.user.role == 'ADMIN'){
@@ -14,5 +14,7 @@ router.use(function(req, res, next) {
 router.get('/', function(req, res){
   res.render('admin/home', {title: 'Admin home'});
 });
+
+router.use('/users',users);
 
 module.exports = router;
